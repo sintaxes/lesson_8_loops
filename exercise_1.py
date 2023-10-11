3e899c8e473bd54173e8ae20d0bb5a6ce0f3c132
Renatas Puodziukas <renatas.puodziukas@gmail.com>
2023-10-11 19:23:31 +0300

    BBZ kas vyksta

--- exercise_1.py
+++ exercise_1.py
@@ -1,12 +1,3 @@
-# i=0
-# while i<10:
-#     print(i)
-#     i=i+1
 
-# ciklas kol bus ivesta reiksme
-# while True:
 
-#     user_input = input("Enter your name: ")
-#     if len(user_input) !=0:
-#         break
-# print(f"Yout enterd {user_input}")
+
