4d792dbcbf363f27fe856c7745e37f8ab8b75c65
Renatas Puodziukas <renatas.puodziukas@gmail.com>
2023-10-11 21:10:41 +0300

    Task1 in lesson 8 Loops

--- exercise_2.py
+++ exercise_2.py
@@ -7,13 +7,32 @@
 # For storing account information use dict , all data should be lowercase. and extra symbols (@, %, $ etc and numbers). 
 # If in password numbers or symbols are not provided, the program should add additional value to dictionary as a value, names 'improved password'
 
-username_input = input("Enter your user name: ")
-userpassword_input = input("Enter your password: ")
+
+print("You will need to enter five user names and passwords")
+
+credentials_list = {}
+
+for enter in range(2):
+    username = input(f"Enter user name {enter+1}: ")
+    password = input(f"Enter user password {enter+1}: ")
+    credentials_list[username] = password
+
+print("Now you will need to enter user name and correct password you eneterd before")
+
+
+while True:
+    login_username = input("Enter your user name: ")
+    login_password = input("enter your password: ")
+
+    if login_username in credentials_list and credentials_list[login_username] == login_password:
+        print("Your credentials are correct")
+        break
+    else:
+        print(" Your credentials are not correct, try again. ")
 
 
 
 
-print(credentials_list)
 
 
 
